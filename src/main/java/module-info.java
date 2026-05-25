35c0a2868fae5445c268615176e8677b7bd991f6
PedroOSanches <pedrohenriqueoliveirasanches3@gmail.com>
2026-05-24 23:16:41 -0300

    fix

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,8 @@
 module br.maua {
-    requires javafx.controls;
-    requires javafx.fxml;
     requires java.sql;
     requires java.dotenv;
     requires java.desktop;
     requires java.logging;
-
-    opens br.maua to javafx.fxml;
-    opens br.maua.presentation to javafx.fxml;
-
     exports br.maua;
     exports br.maua.presentation;
 }
