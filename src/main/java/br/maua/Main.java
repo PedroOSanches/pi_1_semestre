ff5585b5724c3f0028ae6e579d129553ea0deab6
PedroOSanches <pedrohenriqueoliveirasanches3@gmail.com>
2026-05-26 08:34:58 -0300

    Fix: Adiciona tratativa de erro em caso de erro na conexao com o Banco de Dados

--- src/main/java/br/maua/Main.java
+++ src/main/java/br/maua/Main.java
@@ -3,11 +3,15 @@ package br.maua;
 import br.maua.infrastructure.ConnectionFactory;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+        try {
             Connection cx = ConnectionFactory.obterConexao();
-            System.out.println(cx);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
             System.out.println("Projeto Inicializado com Sucesso!");
             java.awt.EventQueue.invokeLater(() -> {
                 new br.maua.presentation.TelaLogin.TelaLogin().setVisible(true);
